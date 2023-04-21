05eaf8408238f234b0fafe979134c672a006d971
Mateusz Karaś <maathi@wp.pl>
2023-04-21 16:47:02 +0200

    Setting up environment

---
+++ gamingHub/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { MasterPage } from './pages';
+
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <MasterPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ gamingHub/src/pages/master-page/MasterPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MasterPage: React.FC = () => { 
+    return (
+        <div> 
+            MasterPage
+        </div>
+    ) 
+
+}
+
