75b928d964a5402efb6c3cc26b92f2bf3200f444
Mateusz Karaś <maathi@wp.pl>
2023-06-02 23:02:16 +0200

    fix

--- gamingHub/src/components/sign-form/SignForm.tsx
+++ gamingHub/src/components/sign-form/SignForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { SignInForm } from "./sign-in";
 import { SignUpForm } from "./sign-up";
 import { Box, Typography, useMediaQuery } from "@mui/material";
