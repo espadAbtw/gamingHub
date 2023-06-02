80001a56a11eab99bc62c76222d6c9e120679ec3
Mateusz Karaś <maathi@wp.pl>
2023-06-02 22:48:12 +0200

    routing, axios config, slice fix, navbar

--- gamingHub/src/pages/master-page/MasterPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { LandingPage } from "../landing-page";
-import { SignForm } from "../../components";
-import { Grid } from "@mui/material";
-
-export const MasterPage: React.FC = () => {
-  return <SignForm />;
-};
